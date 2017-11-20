98b5d287d093913263f2ea28cb527f6cb6bc042d
Sergey Rush <sergey.rush@hotmail.com>
2017-11-20 23:44:09 +0300

    First commit

---
+++ TableViewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewApp
+//
+//  Created by Admin on 20/11/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewApp
+//
+//  Created by Admin on 20/11/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
